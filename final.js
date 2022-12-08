f1cbd0eeca297e475f06d4d448fc7a7d09b500a5
Melika Hamedani <97775710+Melikahamedani@users.noreply.github.com>
2022-12-08 15:11:37 -0500

    Add files via upload

---
+++ final.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
+
+
+//define the user schema
+var finalUsers = new Schema({
+    "email" : {
+        "type" : String,
+        "unique" : true 
+    },
+    "password" : String
+});
+
+
+let User
+
+//start DB
+exports.startDB = () => {
+    return new Promise( (resolve, reject) => {
+        let db = mongoose.createConnection("mongodb+srv://MelikaHamedani:@senecaweb.61idexv.mongodb.net/?retryWrites=true&w=majority");
+        db.on('error', (err) => {
+            console.log("Cannot connect to Database.")
+            reject(err);
+        });
+        db.once('open', () => {
+            User = db.model("finalUsers", finalUsers);
+            console.log("connection successful.")
+            resolve();
+        });
+    });
+};
+
+//register(user)
+exports.register =  (user) => {
+    return new Promise( (resolve, reject) => {
+        if (user.email == '' || user.password == '') {
+            reject('“Error: email or password cannot be empty.”')
+        }
+        bcrypt.genSalt(10)
+            .then(salt => bcrypt.hash(user.password, salt))
+            .then(hash => {
+                user.password = hash
+                let newUser = new User(user);
+                newUser.save().then(() => {
+                    resolve(user)}).catch(err => {
+                    if (err.code == 11000) {
+                        reject(`Error: ${user.email} already exists`)
+                    } else {
+                        reject('Error: cannot create the user')
+                    }
+                })
+            })
+            .catch(err => {
+                return reject(err)
+            });
+    });
+};
+
+//signIn(user)
+exports.signIn = (user) => {
+    return new Promise((resolve, reject) => {
+        User.find({ email: user.email }).then(users => {
+            if (users.length == 0) {
+                return reject(`Unable to find user: ${user.email}`)
+            }
+            bcrypt.compare(user.password, users[0].password).then((res) => {
+                if (res) {
+                    resolve(user)
+                } else {
+                    return reject(` “Incorrect password for user ${user.email}” `)
+                }
+
+            });
+        }).catch(err => reject(`Cannot find the user: ${user.email} `))
+    });
+};
--- server.js
+++ server.js
@@ -1,12 +1,67 @@
+const final = require('./final');
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 8080;
 
 
+//use
+app.use((req, res) => {
+    res.send('not found')
+})
 
 
 //Home route (GET, “/”)
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/finalViews/home.html"));
+});
 
 //GET, “/register”
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,"/finalViews/register.html"));
+});
 
 
 //POST, “/register”
+app.post('/register', (req, res) => {
+    final.register(req.body)
+    .then(data => {
+        if (data.email != undefined) {
+            res.send(`${data.email} registered successfully. <br> <a href="/">Go home </a>`)
+        } else {
+            res.send(`Error:${data}`)
+        }
+    }).catch(err => {
+        res.send(`Error:${err}`)
+    })
+})
 
+//GET, “/signIn”
+app.get('/signIn', (req, res) => {
+    res.sendFile(path.join(__dirname,"/finalViews/signIn.html"));
+});
+
+//POST, “/signIn”
+app.post('/signin', (req, res) => {
+    final.signIn(req.body)
+    .then(data => {
+        res.setHeader('content-type', 'text/html')
+        if (data.email) {
+            res.send(`${data.email} signed in successfully.<br><a href="/">Go home </a>`)
+        } else {
+            res.send(`Error:${data}`)
+        }
+    }).catch(err => res.send(`Error: ${err}`))
+})
+
+//404 Error
+app.get("*", (req, res)=>{
+    res.status(404).send("Error 404: page not found.");
+});
+
+//Start the server
+final.startDB().then(() => {
+    app.listen(HTTP_PORT, onHttpStart);
+    console.log("app is listening port 8080")
+}).catch(() => {
+    console.log("Unable to load data");
+});
